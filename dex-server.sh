96867595bd02a27a4fd48548acce71f6596de40b
Poseidon - Aisland <87597651+poseidon-aisland@users.noreply.github.com>
2023-05-30 07:51:22 +0400

    Update dex-server.sh

--- dex-server.sh
+++ dex-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Api Key from https://0x.org
 export APIKEY=".."
+# the connection data to your Mariadb server (or Mysql Server), please change it accordingly to your configuration.
 export DB_HOST="localhost"
 export DB_NAME=".."
 export DB_USER=".."
