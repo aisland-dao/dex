6badd66422b60eb745b8567d0a4083d571b3c4cc
root <root@dex.aisland.io>
2023-06-01 12:56:08 +0000

    renamed some files for consistency

--- dex-load-main-tokens-coingecko.sh
+++ dex-load-main-tokens-coingecko.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export DB_HOST="localhost"
 export DB_NAME="dex"
-export DB_USER="dex"
-export DB_PWD="88b9-ea46@8e20-5065*1632"
+export DB_USER=".."
+export DB_PWD=".."
 curl https://tokens.coingecko.com/uniswap/all.json >/usr/src/dex/json/tokens-uniswap.json
 node /usr/src/dex/dex-load-main-tokens-coingecko.js
 
