1aef03f2045d3b256fcf1b7a78aa25d9a0deb555
root <root@dex.aisland.io>
2023-06-01 03:46:35 +0000

    removed comment from api call

--- dex-load-coingecko-tokens.sh
+++ dex-load-coingecko-tokens.sh
@@ -3,6 +3,6 @@ export DB_HOST="localhost"
 export DB_NAME=".."
 export DB_USER=".."
 export DB_PWD="...."
-#curl -X 'GET'  'https://api.coingecko.com/api/v3/coins/list?include_platform=true'  -H 'accept: application/json' >/usr/src/dex/json/coingeckocoinslist.json
+curl -X 'GET'  'https://api.coingecko.com/api/v3/coins/list?include_platform=true'  -H 'accept: application/json' >/usr/src/dex/json/coingeckocoinslist.json
 node /usr/src/dex/dex-load-coingecko-tokens.js
 
